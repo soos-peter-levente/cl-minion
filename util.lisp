877127628ae2757c7f2df5ff39752f3f4942242c
spl <soos.peter.levente@protonmail.com>
2017-04-15 11:16:41 +0200

    stream as parameter + various refactors

--- util.lisp
+++ util.lisp
@@ -1,19 +1,27 @@
 (in-package :cl-minion)
 
-(defmacro w/o-wp (stream &body body)
+(defmacro w/o-wp (&body body)
   ;; Wrap the read function definition BODY with peek-char calls,
   ;; so as to strip whitespace characters before and after it.
-  `(progn (peek-char t ,stream nil nil)
+  `(progn (peek-char t *s* nil nil)
           (let ((value ,@body))
-            (peek-char t ,stream nil nil)
+            (peek-char t *s* nil nil)
             value)))
 
-(defun match (char s)
+(defun match (char)
   ;; Make sure we have character CHAR in the stream up next.
   ;; Skip at the same time.
-  (unless (char= char (read-char s nil))
+  (unless (char= char (read-char *s* nil))
     (error "Problem.")))
 
-(defun match-if (char s)
-  (let ((pos (peek-char t s nil nil)))
+(defun match-if (char)
+  ;; Peek ahead one character and decide if CHAR is up next.
+  (let ((pos (peek-char t *s* nil nil)))
     (when pos (when (char= char pos)) pos)))
+
+(defun collect* (fn separator)
+  ;; Call token function FN repeatedly as long as separator
+  ;; is encountered in the input stream between calls.
+  (loop for rep = (match-if separator)
+        :while (and rep (char= rep separator) (read-char *s* nil))
+        :collect (funcall fn)))
